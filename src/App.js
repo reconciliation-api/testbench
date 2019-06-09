452fb14c85b80c28400d3ac9f7107ec5b4803fea
Antonin Delpeuch <antonin@delpeuch.eu>
2019-06-09 13:43:08 +0100

    Main structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap-theme.min.css';
+// import 'bootstrap/dist/css/bootstrap-theme.min.css';
 import FeatureTable from './FeatureTable.js';
 import FeatureRow from './FeatureRow.js';
+import Tabs from 'react-bootstrap/lib/Tabs';
+import Tab from 'react-bootstrap/lib/Tab';
 
-function App() {
-  return (
-    <div className="App">
-        <FeatureTable> 
-           <FeatureRow name="OpenCorporates" endpoint="https://opencorporates.com/reconcile" documentation="https://api.opencorporates.com/documentation/Open-Refine-Reconciliation-API" />
-        </FeatureTable>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App" style={{margin: '20px'}}>
+        <h1>Reconciliation service test bench</h1>
+        <Tabs defaultActiveKey="servicesList" id="main-tabs">
+          <Tab eventKey="servicesList" title="Services">
+            <div style={{'margin-top': '10px'}}>
+                <p>This table lists reconciliation services known to <a href="https://www.wikidata.org/">Wikidata</a> and
+                    indicates which features of the <a href="https://github.com/OpenRefine/OpenRefine/wiki/Reconciliation-Service-API">reconciliation API</a> they support.
+                </p>
+                <FeatureTable> 
+                    <FeatureRow name="OpenCorporates" endpoint="https://opencorporates.com/reconcile" documentation="https://api.opencorporates.com/documentation/Open-Refine-Reconciliation-API" />
+                </FeatureTable>
+            </div>
+          </Tab>
+          <Tab eventKey="testBench" title="Test bench">
+            <div style={{'margin-top': '10px'}}>
+              <p>Hello</p>
+            </div>
+          </Tab>
+        </Tabs>
+      </div>
+    );
+  }
 }
 
-export default App;
--- src/FeatureTable.js
+++ src/FeatureTable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Table from 'react-bootstrap/Table';
+import Table from 'react-bootstrap/lib/Table';
 import FeatureRow from './FeatureRow.js';
 
 export default class FeatureTable extends React.Component {
