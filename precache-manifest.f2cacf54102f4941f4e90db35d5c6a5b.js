cea129df1c15a2a963f5bb7bf7873918ae8ce2f6
Antonin Delpeuch <antonin@delpeuch.eu>
2019-06-21 11:23:29 +0100

    Updates

---
+++ precache-manifest.f2cacf54102f4941f4e90db35d5c6a5b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c80f4b823c95f1ca508b57b77f362ac6",
+    "url": "/testbench/index.html"
+  },
+  {
+    "revision": "009a9b1cccc60c4ef600",
+    "url": "/testbench/static/css/2.8604e687.chunk.css"
+  },
+  {
+    "revision": "e95063c3a35c80bd165f",
+    "url": "/testbench/static/css/main.c206011b.chunk.css"
+  },
+  {
+    "revision": "009a9b1cccc60c4ef600",
+    "url": "/testbench/static/js/2.caebb07c.chunk.js"
+  },
+  {
+    "revision": "e95063c3a35c80bd165f",
+    "url": "/testbench/static/js/main.7b897ff0.chunk.js"
+  },
+  {
+    "revision": "c40a9ccfa148bcc03f62",
+    "url": "/testbench/static/js/runtime~main.09e70599.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/testbench/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/testbench/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/testbench/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/testbench/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/testbench/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
