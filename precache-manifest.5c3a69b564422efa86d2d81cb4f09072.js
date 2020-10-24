93c28b5a65d45ef121344932be2226bbd86e621b
Antonin Delpeuch <antonin@delpeuch.eu>
2020-10-24 14:58:34 +0200

    Updates

--- precache-manifest.5c3a69b564422efa86d2d81cb4f09072.js
+++ precache-manifest.5c3a69b564422efa86d2d81cb4f09072.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "27e041fe4c3d6058f4b23ba1ec426bc0",
+    "revision": "2aa91d29ba36f904942c860d78e33d81",
     "url": "/testbench/index.html"
   },
   {
-    "revision": "22a88d1d79bebfdcc63e",
+    "revision": "aca6ba0a2bc72aa33b15",
     "url": "/testbench/static/css/2.8604e687.chunk.css"
   },
   {
-    "revision": "a1360576086c7d671823",
+    "revision": "a638d4c77d72513e2d86",
     "url": "/testbench/static/css/main.ecb93d9d.chunk.css"
   },
   {
-    "revision": "22a88d1d79bebfdcc63e",
-    "url": "/testbench/static/js/2.0b89ea98.chunk.js"
+    "revision": "aca6ba0a2bc72aa33b15",
+    "url": "/testbench/static/js/2.1e341217.chunk.js"
   },
   {
-    "revision": "a1360576086c7d671823",
-    "url": "/testbench/static/js/main.e81fb6c3.chunk.js"
+    "revision": "a638d4c77d72513e2d86",
+    "url": "/testbench/static/js/main.8a842bad.chunk.js"
   },
   {
     "revision": "c40a9ccfa148bcc03f62",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/testbench/precache-manifest.1c6350dcbd7a159138a3f2ce45a5b605.js"
+  "/testbench/precache-manifest.5c3a69b564422efa86d2d81cb4f09072.js"
 );
 
 self.addEventListener('message', (event) => {
