da8d11c7587eba10b29206874ef406d359d3492f
Antonin Delpeuch <antonin@delpeuch.eu>
2022-06-27 15:27:57 +0200

    Add rendering for candidate features. Closes #37.

--- src/Candidate.js
+++ src/Candidate.js
@@ -17,31 +17,44 @@ export default class Candidate extends React.Component {
      renderDescription() {
         let description = this.props.candidate.description;
         if (description !== undefined) {
-          return (<span><br />{description}</span>);
-        } else {
-           return (<span />);
+          return (<div><div className="candidateField">Description</div><div className="candidateValue">{description}</div></div>);
         }
      }
 
      renderTypes() {
         let types = this.props.candidate.type;
         if (types !== undefined) {
-          return (<span><br />
+          return (<div><div className="candidateField">Types</div><div className="candidateValue">
               {types.map((type, idx) => [
                   idx > 0 && ", ",
                   type.name + ' (' + type.id + ')'
                ])}
-          </span>);
+          </div></div>);
+        }
+     }
+
+     renderFeatures() {
+        let features = this.props.candidate.features;
+        if (features !== undefined) {
+          return (<div>
+                {features.map((feature, idx) => 
+                   (<div key={idx}><div className="candidateField">Feature {feature.id}</div><div className="candidateValue">{feature.value}</div></div>)
+                )}
+             </div>);
         }
      }
 
      render() {
         let candidate = this.props.candidate;
         return (<ListGroupItem key={candidate.id} header={candidate.name} active={candidate.match}>
-                <a href={this.url}>{candidate.id}</a>
                 <Badge style={{float: 'right'}}>{this.props.candidate.score}</Badge>
+                <div>
+                  <div><div className="candidateField">ID</div><div className="candidateValue">
+                <a href={this.url}>{candidate.id}</a></div></div>
                 {this.renderDescription()}
                 {this.renderTypes()}
+                {this.renderFeatures()}
+                </div>
             </ListGroupItem>);
      }
 }
