91d4d5e013d1af8261e708fef2dcef196df06d0a
Abhishek P Anil <abhishek@culturecreates.com>
2025-06-05 12:16:03 +0530

    chore: added version conditional rendering

--- src/Candidate.js
+++ src/Candidate.js
@@ -1,81 +1,102 @@
-
-import React from 'react';
-import Badge from 'react-bootstrap/lib/Badge';
-import ListGroupItem from 'react-bootstrap/lib/ListGroupItem';
+import React from "react";
+import Badge from "react-bootstrap/lib/Badge";
+import ListGroupItem from "react-bootstrap/lib/ListGroupItem";
+import { SPEC_VERSIONS } from "./utils";
 
 export default class Candidate extends React.Component {
+  get url() {
+    const { manifest, candidate } = this.props;
+    return manifest?.view?.url?.replace("{{id}}", candidate?.id ?? "");
+  }
 
-     get url() {
-        let view = null;
-        let manifest = this.props.manifest;
-        if ('view' in manifest && 'url' in this.props.manifest.view && 'id' in this.props.candidate) {
-            view = this.props.manifest.view.url.replace('{{id}}', this.props.candidate.id);
-        }
-        return view;
-     }
+  renderDescription() {
+    const { candidate, version } = this.props;
+    const description = candidate?.description;
 
-     renderDescription() {
-      const description = this.props.candidate?.description;
-  
-      if (description) {
-        return (
-          <div>
-            <div className="candidateField">Description</div>
-            <div className='candidate-description-wrapper'>
-                    <span className="candidate-main-title-str">
-                      {description}
-                    </span>
+    if (!description) return null;
 
-            </div>
+    if (version === SPEC_VERSIONS.DRAFT_1_0) {
+      return (
+        <div>
+          <div className="candidateField">Description</div>
+          <div className="candidate-description-wrapper">
+            <span className="candidate-main-title-str">{description}</span>
           </div>
-        );
-      }
-  
-      return null;
+        </div>
+      );
     }
 
-     renderTypes() {
-        let types = this.props.candidate.type;
-        if (types !== undefined) {
-          return (<div><div className="candidateField">Types</div><div className="candidateValue">
-              {types.map((type, idx) => [
-                  idx > 0 && ", ",
-                  type.name + ' (' + type.id + ')'
-               ])}
-          </div></div>);
-        }
-     }
+    return (
+      <div>
+        <div className="candidateField">Description</div>
+        <div className="candidateValue">{description}</div>
+      </div>
+    );
+  }
 
-     renderFeatures() {
-        let features = this.props.candidate.features;
-        if (features !== undefined) {
-          return (<div>
-                {features.map((feature, idx) => 
-                   (<div key={idx}><div className="candidateField">Feature {feature.id}</div><div className="candidateValue">{feature.value}</div></div>)
-                )}
-             </div>);
-        }
-     }
+  renderTypes() {
+    const types = this.props.candidate?.type;
+    if (!types) return null;
 
-     render() {
-      let candidate = this.props.candidate;
-        return (<ListGroupItem
-         key={candidate.id}
-         header={
-           <span className="candidate-main-title">
-             <span className="candidate-main-title-str-bold">{candidate?.name}</span>
-           </span>
-         }
-         active={candidate.match}
-       >
-                <div>   
-                <Badge style={{float: 'right'}}>{this.props.candidate.score}</Badge>
-                  <div><div className="candidateField">ID</div><div className="candidateValue">
-                <a href={this.url}>{candidate.id}</a></div></div>
-                {this.renderDescription()}
-                {this.renderTypes()}
-                {this.renderFeatures()}
-                </div>
-            </ListGroupItem>);
-     }
+    return (
+      <div>
+        <div className="candidateField">Types</div>
+        <div className="candidateValue">
+          {types.map((type, idx) => [
+            idx > 0 && ", ",
+            `${type.name} (${type.id})`,
+          ])}
+        </div>
+      </div>
+    );
+  }
+
+  renderFeatures() {
+    const features = this.props.candidate?.features;
+    if (!features) return null;
+
+    return (
+      <div>
+        {features.map((feature, idx) => (
+          <div key={idx}>
+            <div className="candidateField">Feature {feature.id}</div>
+            <div className="candidateValue">{feature.value}</div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  render() {
+    const { candidate, version } = this.props;
+    const header =
+      version === SPEC_VERSIONS.DRAFT_1_0 ? (
+        <span className="candidate-main-title">
+          <span className="candidate-main-title-str-bold">
+            {candidate?.name}
+          </span>
+        </span>
+      ) : (
+        candidate?.name
+      );
+
+    return (
+      <ListGroupItem
+        key={candidate.id}
+        header={header}
+        active={candidate.match}
+      >
+        <Badge style={{ float: "right" }}>{candidate.score}</Badge>
+        <div>
+          <div className="candidateField">ID</div>
+          <div className="candidateValue">
+            <a href={this.url}>{candidate.id}</a>
+          </div>
+          {this.renderDescription()}
+          {this.renderTypes()}
+          {this.renderFeatures()}
+        </div>
+      </ListGroupItem>
+    );
+  }
 }
