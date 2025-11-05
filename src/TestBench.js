3ebd438361e33a0d96ea96a0f2f038f4d400ab16
Abhishek P Anil <abhishek@culturecreates.com>
2025-11-05 15:53:49 +0530

    feat: updated the suggest tab for nested property support

--- src/TestBench.js
+++ src/TestBench.js
@@ -19,6 +19,7 @@ import JSONTree from 'react-json-tree';
 import { getSchema } from './JsonValidator.js';
 import { jsonTheme } from './utils.js';
 import PropertyMappingV2 from './PropertyMappingV2.js';
+import PropertyPathInput from './PropertyPathInput.js';
 import { Row } from 'react-bootstrap';
 
 export default class TestBench extends React.Component {
@@ -418,7 +419,7 @@ export default class TestBench extends React.Component {
                     <FormGroup controlId="suggestPropertyTestBench">
                         <Col componentClass={ControlLabel} sm={1}>Property:</Col>
                         <Col sm={11}>
-                            <ReconcileSuggest service={this.props.service} entityClass="property" id="property-suggest-test" />
+                            <PropertyPathInput service={this.props.service} id="property-suggest-test" />
                         </Col>
                     </FormGroup>
                 </Form>
